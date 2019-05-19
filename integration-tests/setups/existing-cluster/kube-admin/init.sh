69a4acb00d622723e7bc0953abfd62c918bf6033
Asma Jabir <azinneera@gmail.com>
2019-05-19 13:26:34 +0530

    Add automation scripts for kubeadm

--- integration-tests/setups/existing-cluster/kube-admin/init.sh
+++ integration-tests/setups/existing-cluster/kube-admin/init.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
+# ------------------------------------------------------------------------
+#
+# Copyright 2019 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+#
+# ------------------------------------------------------------------------
 
-#  Copyright (c) 2019 WSO2 Inc. (http:www.wso2.org) All Rights Reserved.
-#
-#  WSO2 Inc. licenses this file to you under the Apache License,
-#  Version 2.0 (the "License"); you may not use this file except
-#  in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#  http:www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing,
-#  software distributed under the License is distributed on an
-#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-#  KIND, either express or implied.  See the License for the
-#  specific language governing permissions and limitations
-#  under the License.
+node_type=master
 
-# Creates a k8s cluster with kubeadm for installing Cellery
+#Initialize the k8s cluster
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=0.0.0.0
+
+sleep 60
+
+#Create .kube file if it does not exists
+mkdir -p $HOME/.kube
+
+#Move Kubernetes config file if it exists
+if [ -f $HOME/.kube/config ]; then
+    mv $HOME/.kube/config $HOME/.kube/config.back
+fi
+
+cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
+chown -R ubuntu:ubuntu $HOME/.kube
+
+#if you are using a single node which acts as both a master and a worker
+#untaint the node so that pods will get scheduled:
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+#Install Flannel network
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.11.0/Documentation/kube-flannel.yml
+
+echo "Successfully initialized kubeadm."
+
 
