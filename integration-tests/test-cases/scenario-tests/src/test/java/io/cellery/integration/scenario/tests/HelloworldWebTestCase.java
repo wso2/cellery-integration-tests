5fd67efd852c0de80109b67b1c1a7929d5117a39
Anjana Supun <anjanasupun05@gmail.com>
2019-05-31 12:34:22 +0530

    Resolve conflicts

--- integration-tests/test-cases/scenario-tests/src/test/java/io/cellery/integration/scenario/tests/HelloworldWebTestCase.java
+++ integration-tests/test-cases/scenario-tests/src/test/java/io/cellery/integration/scenario/tests/HelloworldWebTestCase.java
@@ -17,17 +17,24 @@
  */
 package io.cellery.integration.scenario.tests;
 
+import io.cellery.integration.scenario.tests.models.Cell;
+import io.cellery.integration.scenario.tests.models.ObservabilityDashboard;
+import io.cellery.integration.scenario.tests.models.SequenceDiagram;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
 
@@ -39,11 +46,20 @@ public class HelloworldWebTestCase extends BaseTestCase {
     private static final String VERSION = "1.0.0";
     private static final String HELLO_WORLD_INSTANCE = "hello-world-inst";
     private WebDriver webDriver;
+    private ObservabilityDashboard observabilityDashboard;
 
     @BeforeClass
     public void setup() {
         WebDriverManager.getInstance(CHROME).setup();
         webDriver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+        observabilityDashboard = new ObservabilityDashboard(webDriver,new WebDriverWait(webDriver,10));
+        observabilityDashboard.setWebCellUrl("http://hello-world.com/");
+        List<String> componentList = new ArrayList<>(Arrays.asList("gateway", "hello"));
+        Cell cell = new Cell(IMAGE_NAME,HELLO_WORLD_INSTANCE,componentList);
+        observabilityDashboard.getCells().add(cell);
+
+        SequenceDiagram diagram = new SequenceDiagram();
+        diagram.getSequenceDiagramCells().put(HELLO_WORLD_INSTANCE,2);
     }
 
     @Test
@@ -61,6 +77,26 @@ public class HelloworldWebTestCase extends BaseTestCase {
     public void invoke() {
         webDriver.get(Constants.DEFAULT_HELLO_WORLD_URL);
         validateWebPage();
+    }
+
+    @Test //(dependsOnMethods = "signIn")
+    public void observabilityLogin () throws InterruptedException {
+        loginObservability(webDriver);
+    }
+
+    @Test (dependsOnMethods = "observabilityLogin")
+    private void overviewPage() {
+        observabilityDashboard.overviewPage();
+    }
+
+    @Test (dependsOnMethods = "overviewPage")
+    private void cellsPage () {
+        observabilityDashboard.cellsPage();
+    }
+
+    @Test (dependsOnMethods = "cellsPage")
+    public void tracingPage () {
+        observabilityDashboard.tracingPage();
     }
 
     @Test
