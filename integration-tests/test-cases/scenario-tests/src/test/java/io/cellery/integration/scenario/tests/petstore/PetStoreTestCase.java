84ee2a01a24fe10600b2d7a4fc88ff076a8470af
MadhukaHarith <madhukaw@wso2.com>
2019-06-13 17:42:24 +0530

    Updated pet-store cell instance termination

--- integration-tests/test-cases/scenario-tests/src/test/java/io/cellery/integration/scenario/tests/petstore/PetStoreTestCase.java
+++ integration-tests/test-cases/scenario-tests/src/test/java/io/cellery/integration/scenario/tests/petstore/PetStoreTestCase.java
@@ -154,8 +154,8 @@ public class PetStoreTestCase extends BaseTestCase {
 
     @Test(description = "This tests the termination of pet-store backend and frontend cells")
     public void terminate() throws Exception {
-//        terminateCell(BACKEND_INSTANCE_NAME);
-//        terminateCell(FRONTEND_INSTANCE_NAME);
+        terminateCell(BACKEND_INSTANCE_NAME);
+        terminateCell(FRONTEND_INSTANCE_NAME);
     }
 
     @Test(description = "This tests the deletion of pet-store backend and frontend cell images")
@@ -168,8 +168,8 @@ public class PetStoreTestCase extends BaseTestCase {
     public void cleanup() {
         webDriver.close();
         try {
-//            terminateCell(BACKEND_INSTANCE_NAME);
-//            terminateCell(FRONTEND_INSTANCE_NAME);
+            terminateCell(BACKEND_INSTANCE_NAME);
+            terminateCell(FRONTEND_INSTANCE_NAME);
         } catch (Exception ignored) {
         }
     }
